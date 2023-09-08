0fe6054c8b9f4714921b82fc790121b2f8dcb98e
whitenight1201 <whitenight1201021@gmail.com>
2023-09-08 07:57:49 -0700

    FE: retype js to jsx file

--- assets/src/components/post/Post.jsx
+++ assets/src/components/post/Post.jsx
@@ -4,8 +4,8 @@ import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
-import CommentForm from '../post/CommentForm';
-import CommentItem from '../post/CommentItem';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading } }) => {
